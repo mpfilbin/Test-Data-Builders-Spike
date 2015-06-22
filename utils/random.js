3ab183664f5a155d623fea5aa91cb12a0aaa289b
Michael Fiblin <michael.filbin@gmail.com>
2015-06-22 09:47:07 -0600

    Modify random to conform to ES6 module spec
    And add support for UUID versions

--- utils/random.js
+++ utils/random.js
@@ -1,12 +1,27 @@
 import uuid from 'uuid'
+import _ from 'lodash'
 
-var random = module.exports = {};
+const supportedUUIDVersions = _.filter(_.keys(uuid), (key) => {
+    "use strict";
+    return key.match('v');
+});
 
-random.integer = (max, min) => {
-
-  return Math.round(random.float(max, min)) / 100;
+export default random = {
+    integer: (max, min) => {
+        "use strict";
+        return Math.round(random.float(max, min)) / 100;
+    },
+    float: (max, min) => {
+        "use strict";
+        return Math.random() * (max - min) + min * 100;
+    },
+    uuid: (version) => {
+        "use strict";
+        if(_.contains(supportedUUIDVersions, version)){
+            return uuid[version]();
+        } else {
+            throw new Error(`Unsupported UUID version: ${version}`);
+        }
+    }
 };
 
-random.float = (max, min) => {
-  return Math.random() * (max - min) + min * 100
-}
